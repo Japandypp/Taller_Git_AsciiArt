f9b88b57b5a23d9116e43e2247915ce2637d5104
Juan Pacheco <jpachecon1@est.ups.edu.ec>
2026-05-04 16:05:01 -0500

    Primer Programa, se añadio un Ascii Art

---
+++ ascii_Primer_Programa.py
@@ -0,0 +1,56 @@
+#-----------------------------------------------------------------------------
+# Script de Python para probar Git
+# Autor: Juan Pacheco <jpachecon1@est.ups.edu.ec>
+# Fecha: 04/05/2026
+#-----------------------------------------------------------------------------
+# Crea un programa que pregunte mi nombre y la edad y luego de un mensaje de bienvenida
+
+def main():
+    print("---------------------------------")
+    print("Ingresemos los datos juntos")
+    print("---------------------------------")
+    
+    nombre = input("Nombre: ")
+    apellido = input("Apellido: ")
+    edad = input("Edad: ")
+    edad = int(edad)
+    
+    print("--------------------")
+    print("Tu informacion es: ")
+    print("Nombre: ", nombre)
+    print("Apellido: ", apellido)
+    print("Edad: ", edad)
+    print("--------------------")
+
+    print("Hola", nombre, "el sistema te da la Bienvenida!!")
+    
+    # Aquí agregamos el ASCII art
+    ascii_art = r"""
+          .  .
+          |\_|\
+          | a_a\
+          | | "]
+      ____| '-\___
+     /.----.___.-'\
+    //        _    \
+   //   .-. (~v~) /|
+  |'|  /\:  .--  / \
+ // |-/  \_/____/\/~|
+|/  \ |  []_|_|_] \ |
+| \  | \ |___   _\ ]_}
+| |  '-' /   '.'  |
+| |     /    /|:  | 
+| |     |   / |:  /\
+| |     /  /  |  /  \
+| |    |  /  /  |    \
+\ |    |/\/  |/|/\    \
+ \|\ |\|  |  | / /\/\__\
+  \ \| | /   | |__
+snd    / |   |____)
+       |_/ 
+"""
+    print(ascii_art)
+
+if __name__ == "__main__":
+    main()
+
